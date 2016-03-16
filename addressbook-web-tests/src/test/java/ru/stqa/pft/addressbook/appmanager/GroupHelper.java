fce9e052b75b1487c87d53c0466735ae00b2de15
Vladyslav Bukach <1mailbox@list.ru>
2016-03-16 22:42:53 +0200

    Реализована проверка и обеспечение выполнения предусловий для Групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -67,6 +67,18 @@ public class GroupHelper extends HelperBase {
     public void submitContactDeletion() {
         wd.switchTo().alert().accept();
     }
+
+    public void createGroup(GroupData group) {
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupPage();
+    }
+
+    public boolean isThereAGroup() {
+        return isElementPresent(By.name("selected[]"));
+
+    }
 }
 //div/div[4]/form[2]/table/tbody/tr[2]/td[1]/input - выбрать контакт
 
